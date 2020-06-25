083965d2b7dd824453d4b15d3475dcae42f5a820
unknown <khodorko88@gmail.com>
2020-06-25 10:48:25 +0300

    production

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,30 +1,42 @@
-import React from 'react';
-import './header.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./header.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    return (
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <Link className="navbar-brand" to="/"><h1>StarDB</h1></Link>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <Link className="navbar-brand" to="/">
+        <h1>StarDB</h1>
+      </Link>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarColor03"
+        aria-controls="navbarColor03"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
 
-                <div className="collapse navbar-collapse" id="navbarColor03">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
-                            <Link to="/people/">People <span className="sr-only">(current)</span></Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/planets/">Planets</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/starships/">Starships</Link>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-    )
+      <div className="collapse navbar-collapse" id="navbarColor03">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <Link to="/people/">
+              People <span className="sr-only">(current)</span>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/planets/">Planets</Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/starships/">Starships</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
--- src/services/swapi-service.js
+++ src/services/swapi-service.js
@@ -1,97 +1,95 @@
 export default class SwapiService {
+  _baseLink = "https://swapi.dev/api";
+  _baseImgUrl = "https://starwars-visualguide.com/assets/img";
 
-    _baseLink = 'https://swapi.co/api';
-    _baseImgUrl = 'https://starwars-visualguide.com/assets/img';
+  getResorse = async (url) => {
+    const res = await fetch(`${this._baseLink}${url}`);
 
-     getResorse = async (url) => {
-        const res = await fetch(`${this._baseLink}${url}`);
-
-        if (!res.ok) {
-            throw new Error(`Could not fetch ${url} recived ${res.status}`);
-        }
-
-        return await res.json();
-    };
-
-     getAllPeople = async () => {
-        const res = await this.getResorse('/people/');
-        return res.results.map(this._transformPerson);
-    };
-
-    getPerson = async (id) => {
-       const res = await this.getResorse(`/people/${id}/`);
-       return this._transformPerson(res);
-    };
-
-    getAllPlanets = async () => {
-        const res = await this.getResorse('/planets/');
-        return res.results.map(this._transformPlanet);
-    };
-
-    getPlanet = async (id) => {
-       const planet = await this.getResorse(`/planets/${id}/`);
-       return this._transformPlanet(planet);
-    };
-
-    getAllStarships = async () => {
-        const res = await this.getResorse('/starships/');
-        return res.results.map(this._transformStarship);
-    };
-
-    getStarship = async (id) => {
-       const res = await this.getResorse(`/starships/${id}/`);
-       return this._transformStarship(res);
-    };
-
-    getPersonImage = ({ id }) => {
-        return `${this._baseImgUrl}/characters/${id}.jpg`
-    };
-
-    getStarshipImage = ({ id }) => {
-        return `${this._baseImgUrl}/starships/${id}.jpg`
-    };
-
-    getPlanetImage = ({ id }) => {
-        return `${this._baseImgUrl}/planets/${id}.jpg`
-    };
-
-    _extractId(item) {
-        const idRegExp = /\/([0-9]*)\/$/;
-        return item.url.match(idRegExp)[1];
+    if (!res.ok) {
+      throw new Error(`Could not fetch ${url} recived ${res.status}`);
     }
 
-    _transformPlanet = (planet) => {
-        return {
-            id: this._extractId(planet),
-            name: planet.name,
-            population: planet.population,
-            rotationPeriod: planet.rotation_period,
-            diameter: planet.diameter
-        }
-    }
+    return await res.json();
+  };
 
-    _transformPerson = (person) => {
-        return {
-            id: this._extractId(person),
-            name: person.name,
-            gender: person.gender,
-            birthYear: person.birth_year,
-            eyeColor: person.eye_color
-        }
-    }
+  getAllPeople = async () => {
+    const res = await this.getResorse("/people/");
+    return res.results.map(this._transformPerson);
+  };
 
-    _transformStarship = (starship) => {
-        return {
-            id: this._extractId(starship),
-            name: starship.name,
-            model: starship.model,
-            manufacturer: starship.manufacturer,
-            costInCredits: starship.cost_in_credits,
-            length: starship.length,
-            crew: starship.crew,
-            passengers: starship.passengers,
-            cargoCapacity: starship.cargo_capacity
-        }
-    }
+  getPerson = async (id) => {
+    const res = await this.getResorse(`/people/${id}/`);
+    return this._transformPerson(res);
+  };
 
+  getAllPlanets = async () => {
+    const res = await this.getResorse("/planets/");
+    return res.results.map(this._transformPlanet);
+  };
+
+  getPlanet = async (id) => {
+    const planet = await this.getResorse(`/planets/${id}/`);
+    return this._transformPlanet(planet);
+  };
+
+  getAllStarships = async () => {
+    const res = await this.getResorse("/starships/");
+    return res.results.map(this._transformStarship);
+  };
+
+  getStarship = async (id) => {
+    const res = await this.getResorse(`/starships/${id}/`);
+    return this._transformStarship(res);
+  };
+
+  getPersonImage = ({ id }) => {
+    return `${this._baseImgUrl}/characters/${id}.jpg`;
+  };
+
+  getStarshipImage = ({ id }) => {
+    return `${this._baseImgUrl}/starships/${id}.jpg`;
+  };
+
+  getPlanetImage = ({ id }) => {
+    return `${this._baseImgUrl}/planets/${id}.jpg`;
+  };
+
+  _extractId(item) {
+    const idRegExp = /\/([0-9]*)\/$/;
+    return item.url.match(idRegExp)[1];
+  }
+
+  _transformPlanet = (planet) => {
+    return {
+      id: this._extractId(planet),
+      name: planet.name,
+      population: planet.population,
+      rotationPeriod: planet.rotation_period,
+      diameter: planet.diameter,
+    };
+  };
+
+  _transformPerson = (person) => {
+    return {
+      id: this._extractId(person),
+      name: person.name,
+      gender: person.gender,
+      birthYear: person.birth_year,
+      eyeColor: person.eye_color,
+    };
+  };
+
+  _transformStarship = (starship) => {
+    return {
+      id: this._extractId(starship),
+      name: starship.name,
+      model: starship.model,
+      manufacturer: starship.manufacturer,
+      costInCredits: starship.cost_in_credits,
+      length: starship.length,
+      crew: starship.crew,
+      passengers: starship.passengers,
+      cargoCapacity: starship.cargo_capacity,
+    };
+  };
 }
