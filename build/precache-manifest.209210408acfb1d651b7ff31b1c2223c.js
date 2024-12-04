d618a838193e57d79ae92449c79dbe471984a6c6
unknown <khodorko88@gmail.com>
2024-12-04 13:24:12 +0200

    buid is ready

---
+++ build/precache-manifest.209210408acfb1d651b7ff31b1c2223c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7d88bd9cc11f7ab6b089d3777a29dbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf03b313e03b83b74e9f",
+    "url": "/static/css/main.849f43d5.chunk.css"
+  },
+  {
+    "revision": "3577eb707f77087b994a",
+    "url": "/static/js/2.fee01343.chunk.js"
+  },
+  {
+    "revision": "dbe5ca7a82c728776bb536c3d2d64135",
+    "url": "/static/js/2.fee01343.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf03b313e03b83b74e9f",
+    "url": "/static/js/main.d7fe48b5.chunk.js"
+  },
+  {
+    "revision": "951347fea0fa77c1dae0",
+    "url": "/static/js/runtime-main.08e0f2e8.js"
+  }
+]);
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,12 +1,12 @@
-import React from "react";
-import "./header.css";
-import { Link } from "react-router-dom";
+import React from 'react';
+import './header.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <Link className="navbar-brand" to="/">
-        <h1>StarDB</h1>
+        <h1>StarDB by Oleksii Khodorko</h1>
       </Link>
       <button
         className="navbar-toggler"
